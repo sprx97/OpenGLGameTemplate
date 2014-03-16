24206312e734ebe00527c0312cac501fecd0b3fd
Jeremy Vercillo <icelax1992@comcast.net>
2014-03-16 16:31:49 -0400

    I think I ACTUALLY got the shaders working this time. Modifying shader code (such as doubling lamInt) affects the scene now).

--- BasicShader.v.glsl
+++ BasicShader.v.glsl
@@ -1,6 +1,12 @@
 #version 120
 
+varying vec3 lightVector;
+varying vec3 normalVector;
+
 void main(void) {
 	gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex;
 	gl_TexCoord[0] = gl_MultiTexCoord0;
+
+	lightVector = normalize(gl_LightSource[0].position.xyz - (gl_ModelViewMatrix * gl_Vertex).xyz);
+	normalVector = normalize( gl_NormalMatrix * gl_Normal );
 } // should just  be a passthrough
